2c04be7141f59948450e70c7c3451ad3f088de5b
Nasidul Islam <nasidul_islam@anfcorp.com>
2019-04-16 13:41:55 -0400

    initial-commit: get rid of bloat.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>Lets just show something first</div>
+    );
+  }
+}
+
+export default App;
