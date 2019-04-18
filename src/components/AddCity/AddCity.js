7dbf782a21e2a098ece310548e34c5c67cf51820
Nasidul Islam <nasidul_islam@anfcorp.com>
2019-04-18 15:36:01 -0400

    step-two: making progress.

---
+++ src/components/AddCity/AddCity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddCity.scss';
+
+class AddCity extends React.Component {
+    render() {
+        return(
+            <div>Add city</div>
+        )
+    }
+}
+
+export default AddCity;
--- src/components/Setup/StepTwo/StepTwo.js
+++ src/components/Setup/StepTwo/StepTwo.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './StepTwo.scss';
 
+// component imports
+import AddCity from '../../AddCity/AddCity';
+
 class StepTwo extends React.Component {
     render() {
         return(
-            <div className="setup-step-two-container">step two</div>
+            <div className="setup-step-two-container">
+                <div className="setup-step-two-top">
+                    <AddCity />
+                </div>
+            </div>
         )
     }
 }
