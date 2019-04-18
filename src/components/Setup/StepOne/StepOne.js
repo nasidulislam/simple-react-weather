841abe0f4e76309674d7eb1b4e42015dbc44728a
Nasidul Islam <nasidul_islam@anfcorp.com>
2019-04-18 12:40:38 -0400

    setup: minor refactor and create button component.

--- src/components/Setup/StepOne/StepOne.js
+++ src/components/Setup/StepOne/StepOne.js
@@ -2,33 +2,37 @@ import React from 'react';
 import './StepOne.scss';
 
 // other imports
+import Button from '../../Button/Button';
 import '../Setup.scss';
 
-class StepOne extends React.Component {
-    render() {
-        return (
-            <div className="setup-step-one-container">
-                <div className="setup-step-one-top">
-                    <span className="step-one-top-content-one">
-                        Hello and welcome to your favorite weather app
-                    </span>
-                    <span className="step-one-top-content-two">
-                        All your weather curiosity at your fingertips
-                    </span>
-                </div>
-
-                <div className="setup-step-one-middle">
-                    <span className="step-one-middle-content">
-                        You can add your cities and locales to your dashboard for easy access
-                    </span>
-                </div>
-
-                <div className="setup-step-one-bottom">
-
-                </div>
+const StepOne = (props) => {
+    return (
+        <div className="setup-step-one-container">
+            <div className="setup-step-one-top">
+                <span className="step-one-top-content-one">
+                    Hello and welcome to your favorite weather app
+                </span>
+                <span className="step-one-top-content-two">
+                    All your weather curiosity at your fingertips
+                </span>
             </div>
-        )
-    }
+
+            <div className="setup-step-one-middle">
+                <span className="step-one-middle-content">
+                    You can add your cities and locales to your dashboard for easy access
+                </span>
+            </div>
+
+            <div className="setup-step-one-bottom">
+                <Button
+                    containerClass="step-one-bottom-content-one"
+                    buttonClass="step-one-continue-button"
+                    label="Lets get started"
+                    variant="primary-button"
+                />
+            </div>
+        </div>
+    )
 }
 
 export default StepOne;
