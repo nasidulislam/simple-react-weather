3b9fe45b61287903ca4da076a91ffcda474adbc2
Nasidul Islam <nasidulislam@gmail.com>
2019-04-20 13:56:07 -0400

    dashboard: build fresh load vs setup flow logic in dashboard mount.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import './Dashboard.scss';
 
+// other imports
+import isEmptyObj from '../core/js/helpers';
+
 class Dashboard extends React.Component {
+    componentDidMount() {
+        let cities = {};
+        if(isEmptyObj(this.props.cities)) {
+            // on a fresh load --> empty state
+            cities = localStorage.getItem("cities");
+        } else {
+	        // coming in through setup steps
+            cities = this.props.cities;
+        }
+    }
+
     render() {
         return(
             <div className="dashboard-container">
