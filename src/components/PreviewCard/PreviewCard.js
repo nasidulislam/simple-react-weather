63f54672a1bf765a0fd035bb04742fb7e849297b
Nasidul Islam <nasidulislam@gmail.com>
2019-04-21 13:41:13 -0400

    preview-card: making progress with styling and information presentation.

--- src/components/PreviewCard/PreviewCard.js
+++ src/components/PreviewCard/PreviewCard.js
@@ -3,12 +3,17 @@ import './PreviewCard.scss';
 
 class PreviewCard extends React.Component {
 	render() {
+		const city = this.props.city;
+		const weatherData = this.props.city.weather;
+
 		return(
 			<div className="preview-card-container">
 				<div className="preview-card-left-content">
-
+					<span>{city.cityName}</span>
 				</div>
-				<div className="preview-card-right-content"></div>
+				<div className="preview-card-right-content">
+					<span>{weatherData.main.temp}</span>
+				</div>
 			</div>
 		)
 	}
