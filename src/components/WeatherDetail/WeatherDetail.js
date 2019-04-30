f195af200411466745c380b2274db05f733b64b7
Nasidul Islam <nasidul_islam@anfcorp.com>
2019-04-30 15:04:42 -0400

    weather-detail: pass selected city to detail component.

---
+++ src/components/WeatherDetail/WeatherDetail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './WeatherDetail.scss';
+
+// other imports
+import content from '../core/js/content';
+
+class WeatherDetail extends React.Component {
+	render() {
+		return(
+			<div className="weather-detail-container">something</div>
+		)
+	}
+}
+
+export default WeatherDetail;
